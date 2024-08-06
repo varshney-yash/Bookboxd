0b9601df9f2367cde42c907757a713aa745c6f8d
Yash Varshney <yash.varshney@careers360.in>
2024-08-06 23:27:45 +0530

    add auth

--- backend/apps/hub/api/controller.py
+++ backend/apps/hub/api/controller.py
@@ -1,7 +1,22 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import requests
 import os
 
 class HomeView(TemplateView):
     pass
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'hub/register.html'
+
+class CustomLoginView(LoginView):
+    template_name = 'hub/login.html' 
+    next_page = '/'
+
+class CustomLogoutView(LogoutView):
+    next_page = '/'
--- backend/urls/hub_urls.py
+++ backend/urls/hub_urls.py
@@ -4,5 +4,8 @@ from django.views.generic.base import RedirectView
 
 hub_paths = [
     path('', RedirectView.as_view(url='/home/', permanent=True)),
-    path('home/', hub_api_views.HomeView, name='book_list')
+    path('home/', hub_api_views.HomeView, name='book_list'),
+    path('register/', hub_api_views.RegisterView.as_view(), name='register'),
+    path('login/', hub_api_views.CustomLoginView.as_view(), name='login'),
+    path('logout/', hub_api_views.CustomLogoutView.as_view(), name='logout'),
 ]
